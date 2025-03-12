d51e0c1c3578d5e403ca07162a938566e36b83cf
Gh0stBy7e <Robin_Rene_Wolf@protonmail.com>
2025-03-12 12:24:35 +0100

    Slight optimisation + compatibility via minSDK (I hope this works)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.browserui.randomahheinkaufsliste"
-        minSdk = 15
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
